a4f45ccc278bf9e7ab0fad125b84e4a894142dc6
Aayush Pal <ap.aayushpal@gmail.com>
2019-12-10 16:19:15 +0530

    added 3rd party sign in button

--- loginscrn/lib/main.dart
+++ loginscrn/lib/main.dart
@@ -29,30 +29,31 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
+        body: Stack(alignment: Alignment.center, children: <Widget>[
       Positioned(
         top: 90.0,
         child: Center(
             child: Column(
-              children: <Widget>[
-                Text('Welcome to', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600)),
-                Text('obodo', style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.w200)),
-              ],
-            )),
+          children: <Widget>[
+            Text('Welcome to',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600)),
+            Text('obodo',
+                style: TextStyle(fontSize: 80.0, fontWeight: FontWeight.w200)),
+          ],
+        )),
       ),
       Positioned(
         top: 180.0,
         child: Container(
           margin: EdgeInsets.only(top: 60),
-          child: Text('Sign In', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w700)),
+          child: Text('Sign In',
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w700)),
         ),
       ),
       Positioned(
         top: 320.0,
         child: Container(
-          width: 280.0,
+            width: 280.0,
             child: TextField(
                 decoration: InputDecoration(
               hintText: 'email',
@@ -67,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
       Positioned(
         bottom: 250.0,
         child: Container(
-          width: 280.0,
+            width: 280.0,
             child: TextField(
                 decoration: InputDecoration(
               hintText: 'password',
@@ -81,8 +82,48 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       Positioned(
         bottom: 180.0,
-        child: Text('Continue with', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),),
-      )
+        child: Text(
+          'Continue with',
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
+        ),
+      ),
+      Positioned(
+        bottom: 50,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.all(Radius.circular(50.0))),
+                padding: EdgeInsets.all(20.0),
+                child: Icon(Icons.access_alarm, size: 40.0),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.all(Radius.circular(50.0))),
+                padding: EdgeInsets.all(20.0),
+                child: Icon(Icons.access_alarm, size: 40.0),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.all(Radius.circular(50.0))),
+                padding: EdgeInsets.all(20.0),
+                child: Icon(Icons.access_alarm, size: 40.0),
+              ),
+            ),
+          ],
+        ),
+      ),
     ]));
   }
 }
